aa0c725e9127e4c0a80171e062b669d0ff480ae2
Adrian <adrian.olbrich1@gmail.com>
2024-07-07 21:48:25 +0200

    make all fields in embed field public

--- src/message/embed/field.rs
+++ src/message/embed/field.rs
@@ -2,7 +2,7 @@ use serde::Serialize;
 
 #[derive(Serialize)]
 pub struct EmbedField {
-    name: String,
-    value: String,
-    inline: Option<bool>,
+    pub name: String,
+    pub value: String,
+    pub inline: Option<bool>,
 }
