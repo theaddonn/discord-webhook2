9a8d4231186596915809ad39385a76da365bf233
Adrian <adrian.olbrich1@gmail.com>
2024-07-08 16:50:40 +0200

    finish Hello World example

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,20 +1,10 @@
-use std::time::SystemTime;
-use discord_webhook2::{DiscordWebhook, Message, message};
+use discord_webhook2::{DiscordWebhook, Message};
 
 #[tokio::main]
 async fn main() {
     let webhook = DiscordWebhook::new(env!("DISCORD_WEBHOOK_URL")).unwrap();
 
-    let time = SystemTime::now();
-
-    let message = Message {
-        content: Some("https://www.youtube.com/watch?v=GFLb5h2O2Ww".to_string()),
-        override_username: None,
-        override_avatar_url: None,
-        tts: None,
-        embeds: None,
-    };
-
-    webhook.send(&message).await;
-
+    webhook.send(&Message::new(|message| message
+        .content("Hello World!")
+    )).await;
 }
