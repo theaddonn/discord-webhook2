52bde2021cab27867cc5ff281233a6d343168329
Adrian <adrian.olbrich1@gmail.com>
2024-07-08 21:32:26 +0200

    add simple embed example

---
+++ examples/simple_embed.rs
@@ -0,0 +1,14 @@
+use discord_webhook2::{DiscordWebhook, Message};
+
+#[tokio::main]
+async fn main() {
+    let webhook = DiscordWebhook::new(env!("DISCORD_WEBHOOK_URL")).unwrap();
+
+    webhook.send(&Message::new(|message| message
+        .embed(|embed| embed
+            .title("Embed Title")
+            .description("description")
+            .url("https://example.com")
+        )
+    )).await;
+}
