9dad79ff662a949b8f2e4d568a427703618edf49
Adrian <adrian.olbrich1@gmail.com>
2024-07-09 14:12:06 +0200

    add other errors

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,16 @@
 use thiserror::Error;
 use url::ParseError;
+use reqwest::Error as ReqwestError;
+use serde_json::Error as JsonError;
 
-#[derive(Error, Debug, Clone)]
+#[derive(Error, Debug)]
 pub enum DiscordWebhookError {
     #[error("URL Parse Error: {0}")]
-    UrlParseError(ParseError)
+    UrlParseError(ParseError),
+
+    #[error("Reqwest Error: {0}")]
+    ReqwestError(ReqwestError),
+
+    #[error("Format Error: {0}")]
+    FormatError(String),
 }
