c9509884b993a9297966651a4159f784cb168b14
Adrian <adrian.olbrich1@gmail.com>
2024-07-10 11:15:15 +0200

    custom ser/de impl

--- src/message/id.rs
+++ src/message/id.rs
@@ -1,4 +1,25 @@
-use serde::{Deserialize, Serialize};
+use std::str::FromStr;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
-#[derive(Serialize, Deserialize, Debug, Clone, Ord, PartialOrd, Eq, PartialEq)]
-pub struct MessageID(pub String);
+#[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct MessageID(pub u64);
+
+impl Serialize for MessageID {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&self.0.to_string())
+    }
+}
+
+impl<'de> Deserialize<'de> for MessageID {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let str = String::deserialize(deserializer)?;
+
+        Ok(Self(u64::from_str(&str).unwrap_or(0)))
+    }
+}
