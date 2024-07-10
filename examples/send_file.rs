539406fdf4054a270b04fc527e6f7c92062217ed
Adrian <adrian.olbrich1@gmail.com>
2024-07-10 10:48:10 +0200

    add send file example

---
+++ examples/send_file.rs
@@ -0,0 +1,20 @@
+use std::collections::BTreeMap;
+use discord_webhook2::{DiscordWebhook, Message};
+use discord_webhook2::embed::image::EmbedImage;
+
+#[tokio::main]
+async fn main() {
+    let webhook = DiscordWebhook::new(env!("DISCORD_WEBHOOK_URL")).unwrap();
+
+    let mut files = BTreeMap::new();
+
+    files.insert(String::from("send_file.png"), Vec::from(include_bytes!("send_file.png")));
+
+    webhook.send_with_files(&Message::new(|message| message
+        .embed(|embed| embed
+            .image(|image| image
+                .url("attachment://send_file.png")
+            )
+        )
+    ), files).await.unwrap();
+}
