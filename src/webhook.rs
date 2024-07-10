91c33537bf547899dc3700caefc9968435e086b6
Adrian <adrian.olbrich1@gmail.com>
2024-07-10 10:48:30 +0200

    add send with files method

--- src/webhook.rs
+++ src/webhook.rs
@@ -1,6 +1,8 @@
+use std::collections::BTreeMap;
+use std::io::Read;
 use reqwest::{Client, Url};
-
-use crate::DiscordWebhookError::{FormatError, ReqwestError};
+use reqwest::multipart::{Form, Part};
+use crate::DiscordWebhookError::ReqwestError;
 use crate::error::DiscordWebhookError;
 use crate::id::MessageID;
 use crate::message::Message;
@@ -34,17 +36,54 @@ impl DiscordWebhook {
             .json(message)
             .send().await;
 
-        let response = send_result.map_err(|e| ReqwestError(e))?;
+        let response = send_result.map_err(|e| DiscordWebhookError::ReqwestError(e))?;
 
-        if response.status().is_success() {
-            let posted_message: Message = response.json::<Message>().await.map_err(|e| ReqwestError(e))?;
+        match response.status().is_success() {
+            true => {
+                let posted_message: Message = response.json::<Message>().await.map_err(|e| ReqwestError(e))?;
 
-            match posted_message.id {
-                None => { return Err(FormatError(String::from("Missing field `id` in response"))) }
-                Some(v) => { return Ok(v) }
+                match posted_message.id {
+                    None => { Err(DiscordWebhookError::FormatError(String::from("Missing field `id` in response"))) }
+                    Some(v) => { Ok(v) }
+                }
             }
-        } else {
-            return Err(FormatError(response.text().await.unwrap().to_string()));
-        };
+            false => {
+                Err(DiscordWebhookError::FormatError(response.text().await.unwrap().to_string()))
+            }
+        }
+    }
+
+    pub async fn send_with_files(&self, message: &Message, files_entries: BTreeMap<String, Vec<u8>>) -> Result<MessageID, DiscordWebhookError> {
+        let mut form = Form::new()
+            .text("payload_json", serde_json::to_string(message).unwrap());
+
+        for (i, (name, data)) in files_entries.into_iter().enumerate() {
+            let mut part = Part::bytes(data);
+
+            part = part.file_name(name);
+
+            form = form.part(format!("files[{i}]"), part);
+        }
+
+        let send_result = self.client
+            .post(self.url.join("?wait=true").unwrap().clone())
+            .multipart(form)
+            .send().await;
+
+        let response = send_result.map_err(|e| DiscordWebhookError::ReqwestError(e))?;
+
+        match response.status().is_success() {
+            true => {
+                let posted_message: Message = response.json::<Message>().await.map_err(|e| ReqwestError(e))?;
+
+                match posted_message.id {
+                    None => { Err(DiscordWebhookError::FormatError(String::from("Missing field `id` in response"))) }
+                    Some(v) => { Ok(v) }
+                }
+            }
+            false => {
+                Err(DiscordWebhookError::FormatError(response.text().await.unwrap().to_string()))
+            }
+        }
     }
 }
