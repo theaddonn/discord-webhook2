7de5e707efe5f2f5d3442d51d96275089de18e93
Adrian <adrian.olbrich1@gmail.com>
2024-07-10 11:57:47 +0200

    add delete message example

---
+++ examples/delete_message.rs
@@ -0,0 +1,16 @@
+use std::time::Duration;
+use tokio::time::sleep;
+use discord_webhook2::{DiscordWebhook, Message};
+
+#[tokio::main]
+async fn main() {
+    let webhook = DiscordWebhook::new(env!("DISCORD_WEBHOOK_URL")).unwrap();
+
+    let message_id = webhook.send(&Message::new(|message| message
+        .content("I'll delete this message again")
+    )).await.unwrap();
+
+    sleep(Duration::from_secs(3)).await;
+
+    webhook.delete(message_id).await.unwrap();
+}
